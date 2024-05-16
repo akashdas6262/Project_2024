683acd96206e7bba42f30a10ab5d79cdca985d33
akashdas6262 <das.akash6262@gmail.com>
2024-05-16 21:50:27 +0530

    Commit trial

---
+++ src/test/java/test.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class test {
+
+
+
+
+@Test
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.myntra.com/");
+        driver.manage().window().maximize();
+        String s = driver.getTitle();
+        System.out.println(s);
+        System.out.println("okok");
+        driver.quit();
+    }
+
+}
